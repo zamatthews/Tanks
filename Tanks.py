4c17e5776a1a0e231cf8a6b0490ab8e01db84346
zamatthews <zamatthews@comcast.net>
2016-02-17 02:10:13 -0500

    Working Missiles, New Guns!

--- Tanks.py
+++ Tanks.py
@@ -6,12 +6,13 @@ import sys
 import math
 import pygame as pg
 from enum import Enum
+from random import randint
 
 CAPTION = "Tanks"
 SCREEN_SIZE = (700, 700)
 BACKGROUND_COLOR = (33, 17, 255)
 BORDER_SIZE = 500
-
+bonus = 0
 
 class gun_type(Enum):
 	basic = 1
@@ -35,6 +36,7 @@ class Tank(object):
 		self.turn_speed = 1
 		self.move_speed = 1
 		self.bullet_timer = 0;
+		self.gun = gun_type.basic
 		
 	def spin_turret(self):
 		self.turret_angle += self.rotate_speed*self.turret_direction
@@ -77,22 +79,58 @@ class Tank(object):
 		if keys[pg.K_RIGHT]:
 			self.turret_direction = -1
 		if keys[pg.K_SPACE] and self.bullet_timer == 0:
+			self.add_bullet(bullets)
 			bullets.add(bullet(self.turret_rect.center, self.turret_angle))
 			self.bullet_timer = 60
 		if self.bullet_timer > 0:
 			self.bullet_timer -= 1
+			
+	def add_bullet(self, bullets):
+		if self.gun == gun_type.basic:
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle))
+		if self.gun == gun_type.advanced:
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle - 15))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle + 15))
+		if self.gun == gun_type.ultra:
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle - 15))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle + 15))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle - 25))
+			bullets.add(bullet(self.turret_rect.center, self.turret_angle + 25))
+			
 	def update(self, screen_rect):
-	
 		self.spin_turret();
 		self.spin_tank();
 		self.move_tank(screen_rect);
 		self.turret_direction = 0
 		self.tank_spin_direction = 0
 		self.tank_move_direction = 0
+		global bonus 
+		if bonus < 10:
+			gun = gun_type.basic
+		elif bonus < 25:
+			self.gun = gun_type.advanced
+		else:
+			self.gun = gun_type.ultra
 		
 	def draw(self, surface):
 		surface.blit(self.tank, self.tank_rect);
 		surface.blit(self.turret, self.turret_rect);
+		if not self.gun == gun_type.basic:
+			extra_turret = pg.transform.rotate(self.turret_start, self.turret_angle + 15)
+			extra_rect = extra_turret.get_rect(center = self.turret_rect.center)
+			surface.blit(extra_turret, extra_rect);
+			extra_turret = pg.transform.rotate(self.turret_start, self.turret_angle - 15)
+			extra_rect = extra_turret.get_rect(center = self.turret_rect.center)
+			surface.blit(extra_turret, extra_rect);
+		if self.gun == gun_type.ultra:
+			extra_turret = pg.transform.rotate(self.turret_start, self.turret_angle + 25)
+			extra_rect = extra_turret.get_rect(center = self.turret_rect.center)
+			surface.blit(extra_turret, extra_rect);
+			extra_turret = pg.transform.rotate(self.turret_start, self.turret_angle - 25)
+			extra_rect = extra_turret.get_rect(center = self.turret_rect.center)
+			surface.blit(extra_turret, extra_rect);
 
 class bullet(pg.sprite.Sprite):
 	def __init__(self, location, angle):
@@ -111,6 +149,51 @@ class bullet(pg.sprite.Sprite):
 	def remove(self, screen_rect):
 		if not self.rect.colliderect(screen_rect):
 			self.kill()
+	def get_rect(self):
+		return self.rect
+			
+class missile_type(Enum):
+	easy = 1
+	normal = 2
+	hard = 3
+	
+class missile(pg.sprite.Sprite):
+	def __init__(self, location, angle, type):
+	
+		pg.sprite.Sprite.__init__(self)
+		self.angle = angle
+		self.type = type
+		
+		if type == missile_type.easy:
+			self.image = pg.transform.rotate(MISSILE_1, angle)
+			self.speed = 0.5
+		if type == missile_type.normal:
+			self.image = pg.transform.rotate(MISSILE_2, angle)
+			self.speed = 1
+		if type == missile_type.hard:
+			self.image = pg.transform.rotate(MISSILE_3, angle)
+			self.speed = 0.75
+			
+		self.pos = location
+		self.rect = self.image.get_rect(center=location)
+		self.done = False;
+		
+	def update(self, screen_rect):
+		self.pos = (self.pos[0] + self.speed*math.sin(math.radians(self.angle + 90)), self.pos[1] + self.speed*math.cos(math.radians(self.angle + 90)))
+		self.rect = self.image.get_rect(center = self.pos)
+		self.remove(screen_rect)
+	def check_shot(self, bullet_rect):
+		if self.rect.contains(bullet_rect):
+			self.kill()
+			global bonus
+			bonus += 1
+			return True
+	def remove(self, screen_rect):
+		if not self.rect.colliderect(screen_rect):
+			self.kill()
+			global bonus
+			bonus -= 1;
+		
 		
 class Driver(object):
 	def __init__(self):
@@ -120,19 +203,34 @@ class Driver(object):
 		self.keys = pg.key.get_pressed()
 		self.player = Tank((SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2))
 		self.bullets = pg.sprite.Group()
+		self.missiles = pg.sprite.Group()
 		self.island = ISLAND
 		self.island_rect = self.island.get_rect(center =(SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2))
+		self.frequency = 1024
+		self.goal_amount = 4
+		self.missile_chance = (100, 0, 0)
+		self.missile_tick = 0;
+		self.score = 0;
 
 	def update_objects(self):
+		self.check_collisions()
 		self.player.update(self.island_rect)
 		self.bullets.update(self.screen_rect)
+		self.missiles.update(self.screen_rect)
 		
 	def draw_objects(self):
 		self.screen.fill(BACKGROUND_COLOR)
 		self.screen.blit(self.island, self.island_rect)
 		self.bullets.draw(self.screen)
 		self.player.draw(self.screen)
+		self.missiles.draw(self.screen)
 		
+	def check_collisions(self):
+		for bullet in self.bullets:
+			for missile in self.missiles:
+				if missile.check_shot(bullet):
+					self.score += 1
+	
 	def game_loop(self):
 		while not self.done:
 			for event in pg.event.get():
@@ -143,7 +241,52 @@ class Driver(object):
 			self.update_objects()
 			self.draw_objects()
 			pg.display.flip()
+			if self.missile_tick > self.frequency:
+				self.add_missile()
+				self.missile_tick = 0
+			self.missile_tick += 1
+			if(self.score >= self.goal_amount):
+				self.new_wave()
+	def add_missile(self):
+		side = randint(1, 4)
+		side_pos = randint(0, SCREEN_SIZE[0]);
+		print(self.score)
+		if side == 1:
+			location = (0, side_pos)
+			angle = 0
+		if side == 2:
+			location = (SCREEN_SIZE[0], side_pos)
+			angle = 180
+		if side == 3:
+			location = (side_pos, 0)
+			angle = 270
+		if side == 4:
+			location = (side_pos, SCREEN_SIZE[1])
+			angle = 90
+		rand = randint(1, 100)
 		
+		if rand < self.missile_chance[0]:
+			type = missile_type.easy
+		elif rand < self.missile_chance[0] + self.missile_chance[1]:
+			type = missile_type.normal
+		else:
+			type = missile_type.hard
+		self.missiles.add(missile(location, angle, type))		
+			
+	
+	def new_wave(self):
+		self.frequency /= 1.5
+		self.goal_amount *= 2
+		missile_3_chance = self.missile_chance[2] + self.missile_chance[1] / 3
+		missile_2_chance  = self.missile_chance[1] * 2 / 3
+		missile_2_chance += self.missile_chance[0] / 3
+		missile_1_chance = self.missile_chance[0] * 2 / 3
+		self.missile_chance = (missile_1_chance, missile_2_chance, missile_3_chance);
+		print(missile_3_chance)
+		print(missile_2_chance)
+		print(missile_1_chance)
+
+			
 if __name__ == "__main__":
 	os.environ['SDL_VIDEO_CENTERED'] = '1'
 	pg.init()
@@ -153,6 +296,9 @@ if __name__ == "__main__":
 	TANK = pg.transform.scale(pg.image.load("Textures/Tank_Base.png").convert_alpha(), (50, 50))
 	BULLET = pg.transform.scale(pg.image.load("Textures/Bullet.png").convert_alpha(), (40, 40))
 	ISLAND = pg.transform.scale(pg.image.load("Textures/Island.png").convert_alpha(), (BORDER_SIZE, BORDER_SIZE))
+	MISSILE_1 = pg.transform.scale(pg.image.load("Textures/Missile_1.png").convert_alpha(),(100, 100))
+	MISSILE_2 = pg.transform.scale(pg.image.load("Textures/Missile_2.png").convert_alpha(),(100, 100))
+	MISSILE_3 = pg.transform.scale(pg.image.load("Textures/Missile_3.png").convert_alpha(),(100, 100))
 	run = Driver()
 	run.game_loop()
 	pg.quit()
